d43db45c5347e4b7cae972d14e60d15553f1c928
Ruben Sanchez <rubensanchezcolin@gmail.com>
2024-01-20 23:52:40 -0600

    Build Production - Plugin

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '587eac2bb6284bf700c3');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '3d7041029353dfc32354');
